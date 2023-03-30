1f075ae561e0367bd6011e2deeb192c5e710df92
Nikita Norseev <nikita.norseev@gmail.com>
2023-03-30 21:43:17 +0300

    asdf

--- src/index.js
+++ src/index.js
@@ -14,14 +14,16 @@ import {
     imagePopupSelector,
     userNameSelector,
     userAboutSelector,
-} from './constants.js';
+} from './scripts/constants.js';
 
-import {Card} from './Card.js';
-import {FormValidator} from './FormValidator.js';
-import {PopupWithImage} from "./PopupWithImage.js";
-import {PopupWithForm} from "./PopupWithForm.js";
-import {Section} from "./Section.js";
-import {UserInfo} from "./UserInfo.js";
+import {Card} from './scripts/Card.js';
+import {FormValidator} from './scripts/FormValidator.js';
+import {PopupWithImage} from "./scripts/PopupWithImage.js";
+import {PopupWithForm} from "./scripts/PopupWithForm.js";
+import {Section} from "./scripts/Section.js";
+import {UserInfo} from "./scripts/UserInfo.js";
+
+import './pages/index.css';
 
 
 // --------------- Создание карточек
